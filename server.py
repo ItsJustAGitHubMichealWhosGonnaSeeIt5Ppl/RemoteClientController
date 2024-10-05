522c513892e2ed3db359c6c154eb601da9950e64
BritishCopsAreClowns <finnryanit@gmail.com>
2024-10-05 01:58:53 +0100

    Added input validation FIXES Make server IP and port changeable in program #2

--- server.py
+++ server.py
@@ -1,25 +1,104 @@
 import socket
 import threading
 import time
+import re
 from flask import Flask, render_template, request
 from waitress import serve
 
 
-#TODO #4 create web interface for actions
 
-# Users connected
+#TODO #2 Make server IP and port changeable in program
+# Server info
+
+
+def inputValidator(message, validInputs, maxAttemps=10,exitOnFailure=True):
+    """Basic input validation function.
+
+    Args:
+        message (string): Message to be sent requesting user input.
+        validInputs (list): list of valid inputs. Not case sensitive.
+        maxAttemps (int, optional): Maximum attempts allowed. Defaults to 10.
+        exitOnFailure (bool, optional): Specifies whether entire program should close if no valid user input is given. Defaults to True.
+
+    Returns:
+        string: user input or failure code.
+    """
+    validInput = False
+    attempts = 0
+    while validInput == False and attempts < maxAttemps:
+        usrInp = input(message)
+        if usrInp.lower() in validInputs.lower():
+            return usrInp
+        else:
+            print('Invalid response')
+            attempts +=1
+
+    if exitOnFailure == True:
+        print('Max attempts reached, stopping program')
+        exit()
+    else:
+        print('Max attempts reached, skipping')
+        return 'maxAttemptsReached'
+        
+
+# I know I could use configparser, but I don't want more modules
+print('Trying to load details from config')
+try:
+    config = open('config.txt', 'r')
+    for line in config:
+        if line.startswith('IP'):
+            host = line.replace('IP=','')
+        elif line.startswith('PORT'):
+            port = int(line.replace('PORT=',''))
+    config.close()
+    configFound = True
+
+except:
+    configFound = False
+
+# Offer to load existing config
+if configFound == True:
+    print(f'Existing config found\nIP/HOST: {host}\nPORT: {port}\n Would you like to use it?')    
+    userResponse = inputValidator('Y/N: ',['y','yes','n','no'])
+    if userResponse.lower().startswith('n'):
+        print('Enter desired IP and port for the server')
+        host = input('IP/Host: ')
+        port = input('Port: ')
+
+
+
+#TODO #12 add better input validation here
+# Start server
+try:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port)) 
+except:
+    print('Server failed to start')
+    exit()
+
+
+# Offer to save config
+print('Would you like to save the current server settings?')
+userResponse = inputValidator('Y/N: ',['y','yes','n','no'],10,False)
+if userResponse ==' maxAttemptsReached':
+    pass
+elif userResponse.lower().startswith('n'):
+    print('config not saved')
+else:
+    try:
+        config = open('config.txt', 'w')
+        config.write(f"IP={host}\nPORT={port}")
+        config.close()
+    except:
+        print('failed to save config, try again later')
+
+
+#TODO #13 Make username and client list into dictionary
+# Users
 clientsList = []
 #Usernames 
 usernamesList = []
 
-#TODO #2 Make server IP and port changeable in program
-# Server info
-host = '10.9.8.54'
-port = 36950
-
-# Start server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port)) 
 
 #Maximum clients
 server.listen(20)
@@ -40,12 +119,12 @@ def lifeCheck(client):
             if client.recv(1024).decode('ascii') == 'YES':
                 continue
             else:
-                print(client)
                 index = clientsList.index(client)
                 clientsList.remove(client)
                 client.close()
                 nickname = usernamesList[index]
                 usernamesList.remove(nickname)
+                print(f'{nickname} disconnected (timeout)')
                 break
         except:
             print(client)
@@ -55,6 +134,7 @@ def lifeCheck(client):
             client.close()
             nickname = usernamesList[index]
             usernamesList.remove(nickname)
+            print(f'{nickname} disconnected (exception)')
             break
         
 
@@ -64,7 +144,7 @@ def receive():
     while True:
         # Accept Connection
         client, address = server.accept()
-        print("Connected with {}".format(str(address)))
+        print("New user connected from {}".format(str(address)))
 
         # Request And Store Nickname
         client.send('IDENT'.encode('ascii'))
@@ -78,16 +158,17 @@ def receive():
         # Start Handling Thread For Client
         thread = threading.Thread(target=lifeCheck, args=(client,))
         thread.start()
-        
+
+
+
 # Send commands
 #TODO #10 This doesn't need to be a thread, convert to be used as needed.
 # TODO #11 Switch command sending to use json.loads and json.dumps
 def sendCommand(command,interactive,*extra):
-    while True:
-        if command == 'STOP':
-            server.close()
-            break
-        broadcast(command)
+    
+    if command == 'STOP':
+        server.close()
+    broadcast(command)
 
 
 # Start thread
@@ -113,7 +194,7 @@ def main():
     if request.method == 'POST':
         incentiveName = list(request.form.listvalues())[0][0]
         print(f'{incentiveName} pressed')
-        sendCommand(incentiveName, interactive = False)
+        sendCommand(incentiveName+ "()", interactive = False)
         
     return render_template('control.html',items=incentives)
 
