93389bb8433830dc38e25292457081877a684207
Finn <finnryanit@gmail.com>
2024-11-11 17:32:19 +0000

    Rewrote to use external JSON file for incentive information instead of storing it inside the function. Removed unneeded dictionary creator functions.

--- incentiveFiles/truckIncentives.py
+++ incentiveFiles/truckIncentives.py
@@ -2,6 +2,7 @@ from pynput.keyboard import Key, Controller
 from playsound3 import playsound
 import time
 import random
+import json
 
 #TODO #9 Get TTS working
 #TODO #17 Mark conflicting functions
@@ -11,138 +12,109 @@ activeFunctions = ["parkingBrake","slalom","everyCam","cinematicCam","metalPipes
 
 keyboard = Controller()
 
-def templateIncentive(details=False):
-    # FLOAT! Incentive Cost.  Used if ExtraLife integration is enabled
-    cost = 12.3
-    # STRING! Friendly name. Displayed in control panel
-    fName = 'name'
+# # # Keypresses
+
+def parkingBrake():
+    """Enable parking brake
+    """
+
+    keyboard.press(Key.space)
+    time.sleep(.5)  
+    keyboard.release(Key.space)
+
+
+def slalom(delay=.3):
+    """Zig zag back and forth"""
+    loop = 0
+    while loop < 5:
+        keyboard.press('a')
+        time.sleep(delay)
+        keyboard.release('a')
+        time.sleep(.5)
+        keyboard.press('d')
+        time.sleep(delay)
+        keyboard.release('d')
+        time.sleep(.5)
+        loop+=1
+        
+
+def towToService():
+    """Tow to nearest service centre
+    """
+
+    keyboard.tap('f')
+    keyboard.tap('e')
+    keyboard.tap(Key.space)
+    time.sleep(2)
+    keyboard.tap(Key.f7)
+    time.sleep(.5)
+    keyboard.press(Key.enter)
+    time.sleep(.2)
+    keyboard.release(Key.enter)
+    time.sleep(.2)
+    keyboard.tap('1')
+    time.sleep(1)
+    keyboard.press(Key.enter)
+    time.sleep(.2)
+    keyboard.release(Key.enter)
+
     
+# # # Camera modifiers
+def everyCam():
+
+    loop = 0
+    while loop < 7:
+        num = str(random.randrange(0,9))
+        keyboard.tap(num)
+        loop+=1
+        time.sleep(.4)
+    keyboard.tap('1')
+
+
+def cinematicCam():
+    """Enable cinematic camera (20 seconds)
+    """
+
+    count = 0
+    while count < 5:
+        keyboard.tap('8')
+        time.sleep(5) 
+        count +=1
+    keyboard.tap('1')
+
+# # # Sounds
+# TODO #8 Get sounds from the web working
+def metalPipes():
+    """Play metal pipes sound
+    """
+    playsound('sounds/metalPipe.mp3')
+
+
+# # # No actions
+def wrongSide(): # No action to be performed
+    cost = 9.27
+    fName = 'Innit'
+    incntID = 'F4C535C1-0E5E-1C62-AB2D979F8A51E076'
+    
+    if details == True:
+        return cost, fName, incntID
+    #TODO add sound
+
+def hitCop():
+    # FLOAT! Incentive Cost.  Used if ExtraLife integration is enabled
+    cost = 9.11
+    # STRING! Friendly name. Displayed in control panel
+    fName = 'I fought the law and I won'
+    incntID = 'F4BEC855-F997-3EE8-077CC68E98073914'
     # Returns details
     if details == True:
-        return cost, fName
+        return cost, fName, incntID
     else:
         # Run action here
         pass
 
 
-# # # Keypresses
-
-def parkingBrake(details=False):
-    """Enable parking brake
-    """
-    cost = 6.0
-    fName = 'Throw \'er in park'
-    
-    if details == True:
-        return cost, fName
-    else:
-        keyboard.press(Key.space)
-        time.sleep(.5)  
-        keyboard.release(Key.space)
-
-
-def slalom(details=False):
-    cost = 3.0
-    fName = 'Safety Car'
-    
-    if details == True:
-        return cost, fName
-    else:
-        delay = .3
-        loop = 0
-        while loop < 5:
-            keyboard.press('a')
-            time.sleep(delay)
-            keyboard.release('a')
-            time.sleep(.5)
-            keyboard.press('d')
-            time.sleep(delay)
-            keyboard.release('d')
-            time.sleep(.5)
-            loop+=1
-        
-
-#TODO #5 confirm this works
-def towToService(details=False):
-    """Tow to nearest service centre
-    """
-    cost = 50.0
-    fName = 'If it ain\'t broke, fix it'
-    
-    if details == True:
-        return cost, fName
-    else:
-        keyboard.tap('f')
-        keyboard.tap('e')
-        keyboard.tap(Key.space)
-        time.sleep(2)
-        keyboard.tap(Key.f7)
-        time.sleep(.5)
-        keyboard.press(Key.enter)
-        time.sleep(.2)
-        keyboard.release(Key.enter)
-        time.sleep(.2)
-        keyboard.tap('1')
-        time.sleep(1)
-        keyboard.press(Key.enter)
-        time.sleep(.2)
-        keyboard.release(Key.enter)
-
-    
-# # # Camera modifiers
-
-def everyCam(details=False):
-    cost = 13.37
-    fName = 'Spam cam'
-    
-    if details == True:
-        return cost, fName
-    else:
-        loop = 0
-        while loop < 7:
-            num = str(random.randrange(0,9))
-            keyboard.tap(num)
-            loop+=1
-            time.sleep(.4)
-        keyboard.tap('1')
-
-
-def cinematicCam(details=False):
-    """Enable cinematic camera (20 seconds)
-    """
-    cost = 5.0
-    fName = 'Goto view'
-    
-    if details == True:
-        return cost, fName
-    else:
-        count = 0
-        while count < 5:
-            keyboard.tap('8')
-            time.sleep(5) 
-            count +=1
-        keyboard.tap('1')
-
-
-
-# # # Sounds
-# TODO #8 Get sounds from the web working
-
-def metalPipes(details=False):
-    """Play metal pipes sound
-    """
-    cost = 17.0
-    fName = 'Did you hear something?'
-    
-    if details == True:
-        return cost, fName
-    else:
-        playsound('sounds/metalPipe.mp3')
-
-
-
 # # # Other
-
 #TODO #6 allow function keys, windows key, etc to be used through here.
 """ 
 def anyKey(key):
@@ -157,34 +129,24 @@ def openLink(link):
         pass
     
  """
+ 
+ 
+ # List creator
+def incentiveDetails():
+    """Generates two dictionaries for price and incentive IDs
 
-def createIncentiveDict():
-    global activeFunctions
-    incentives = {}
-    # Run and collect function information
-    for item in activeFunctions:
-        resultDict = {}
-        exec(f'itemDetails = {item}(details=True)',globals(),resultDict)
-        resultItems = resultDict['itemDetails']
-        incentives.update({
-            f'{item}': {
-                'cost': resultItems[0],
-                'name': resultItems[1]
-                    }
-       })
-    print(incentives)
-    return incentives
-
-def createPriceDict(): # Price dictionary
-    global activeFunctions
-    incentives = {}
-    # Run and collect function information
-    for item in activeFunctions:
-        resultDict = {}
-        exec(f'itemDetails = {item}(details=True)',globals(),resultDict)
-        resultItems = resultDict['itemDetails']
-        incentives.update({
-            resultItems[0] : [resultItems[1],item]
-       })
-    print(incentives)
-    return incentives
+    Returns:
+        list: Two dictionaries.  1 Prices, 2 Incentive IDs
+    """
+    priceDict = {}
+    incntIDDict = {}
+    with open('truckIncentives.json', 'r') as jsonFile:
+        incentives = json.load(jsonFile)
+    for incentive in incentives:
+        priceDict.update({incentive['Amount']: [incentive['Name'],incentive['Command']]})
+        if incentive['IncentiveID'] != '': # Skip blank incentive IDs
+            incntIDDict.update({incentive['IncentiveID']: [incentive['Name'],incentive['Command']]})
+        else:
+            continue
+    return priceDict, incntIDDict
+    
